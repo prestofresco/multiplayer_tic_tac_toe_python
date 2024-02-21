77182c6c7995be6f6da295b746a535bc31732b4a
prestofresco <prestonkuhn@gmail.com>
2024-02-20 19:58:52 -0600

    first commit

---
+++ client.py
@@ -0,0 +1,31 @@
+import tkinter
+import socket
+import sys
+
+
+class User:
+    def __init__(self, username, player_role) -> None:
+        self.username = username
+        self.player_role = player_role # this will be either "X" or "O"
+
+
+class TicTacToe:
+    def __init__(self) -> None:
+        self.gameboard = ["", "", "", "", "", "", "", "", ""] # array of game board slot states.
+
+
+def establish_connection():
+    # TODO
+    client_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    
+
+
+# ------------------ MAIN --------------------
+def main():
+    print()
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+import threading
+
+
+def establish_connection():
+    # TODO
+    client_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+# ------------------ MAIN --------------------
+def main():
+    print()
+    
+
+
+if __name__ == "__main__":
+    main()
