cd48d792af8fb09377d9c86b6e1e5e66ccf7895b
prestofresco <prestonkuhn@gmail.com>
2024-03-06 20:51:34 -0600

    chat implemented using json objects sent via sockets

--- client.py
+++ client.py
@@ -3,7 +3,20 @@
 import tkinter
 import socket
 import sys
+import threading
+import json
 
+HOST = '127.0.0.1'
+PORT = 5200
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+user = None
+help_menu = "\n--------------------------------- HELP MENU ----------------------------------------\n"
+help_menu += "Type a message to chat with other players.\n"
+help_menu += "If you would like to start a game with another player type: 'play' in chat.\n"
+help_menu += "To display the help menu type: 'help' in chat.\n"
+help_menu += "To display active users type: 'users' in chat.\n" # TODO
+help_menu += "------------------------------------------------------------------------------------\n"
 
 class User:
     def __init__(self, username, player_role) -> None:
@@ -13,21 +26,67 @@ class User:
 
 class TicTacToe:
     def __init__(self) -> None:
-        self.gameboard = ["", "", "", "", "", "", "", "", ""] # array of game board slot states.
+        self.gameboard = [ ["", "", ""], ["", "", ""], ["", "", ""] ] # array of game board slot states.
 
 
 def establish_connection():
-    # TODO
-    client_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+    global user
+    username = input("Choose your username: ")
+    # role = input("choose your game piece ('X' or 'O')")
+    role = ""
+    global user
+    user = User(username, role)
+    user_message = {'username': user.username}
+    user_message = json.dumps(user_message)
+    client_socket.sendall(user_message.encode('utf-8'))
+    print("\nYou are connected to the chat room!")
+    print(help_menu)
     
+
+def receive():
+    while True:
+        try:
+            message = client_socket.recv(4096).decode('utf-8')
+            message = json.loads(message)
+
+            if 'chat' in message:
+                print(message['chat'])
+
+        except Exception as error:
+            print("An error occurred!", error)
+            client_socket.close()
+            break
+
+def write():
+    while True:
+        message = input("") # get chat message input
+
+        if message.lower() == 'play':
+            start_game_msg = {'startgame': user.username}
+            start_game_msg = json.dumps(start_game_msg) # serialized json
+            client_socket.sendall(start_game_msg.encode('utf-8'))
+        
+        elif message.lower() == 'help':
+            print(help_menu)
+       
+        else: # just a chat message
+            message = f'{user.username}: {message}'
+            chat_msg = {'chat': message}
+            chat_msg = json.dumps(chat_msg) # serialized json
+            client_socket.sendall(chat_msg.encode('utf-8'))
+
+
 
 
 
 # ------------------ MAIN --------------------
 def main():
-    print()
-    
+    establish_connection()
+    threading.Thread(target=receive).start() # start the receive thread
+    threading.Thread(target=write).start() # start the write thread
 
 
 if __name__ == "__main__":
     main()
+    
--- server.py
+++ server.py
@@ -1,17 +1,84 @@
 import socket
 import sys
 import threading
+import json
+from client import User, TicTacToe
+
+HOST = '127.0.0.1'
+PORT = 5200
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+clients = [] # array of connected clients
+users = [] # array of connected usernames
+
+class GameInstance:
+    def __init__(self, user, tictactoe) -> None:
+        self.users = []
+        self.tictactoe = TicTacToe()
 
 
 def establish_connection():
     # TODO
-    client_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+    print(f"Server is running on {HOST}:{PORT}")
+
+
+def send_chat_all(message):
+    for client in clients:
+        chat_msg = {'chat': message}
+        chat_msg = json.dumps(chat_msg) # serialized json
+        client.sendall(chat_msg.encode('utf-8'))
+
+
+# handle client interactions
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(4096).decode('utf-8')
+            message = json.loads(message)
+            if 'chat' in message:
+                send_chat_all(message['chat'])
+
+            elif 'startgame' in message:
+                start_msg = {'chat': "game start initiated! (not implemented yet)"}
+                start_msg = json.dumps(start_msg) # serialized json
+                client.sendall(start_msg.encode('utf-8'))
+                
+
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            username = users[index]
+            send_chat_all(f"* {username} disconnected! *")
+            users.remove(username)
+            break
+
+
+def receive_new_client():
+    while True:
+        client, address = server_socket.accept()
+        print(f"New connection with {str(address)}")
+
+        # handle the new user's username choice
+        username_response = client.recv(4096).decode('utf-8')
+        username_response = json.loads(username_response)
+        users.append(username_response['username'])
+        clients.append(client)
+
+        print(f"New client connected! Username: {username_response['username']}!")
+        send_chat_all(f"'{username_response['username']}' joined the server!")
+
+        # start the thread to handle the client interactions
+        threading.Thread(target=handle_client, args=(client,)).start()
 
 
 
 # ------------------ MAIN --------------------
 def main():
-    print()
+    establish_connection()
+    receive_new_client()
     
 
 
